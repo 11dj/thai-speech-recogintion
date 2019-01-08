37ed040b5a6ddf832dec850a3cc87ee27b8e2792
kittist <kittisq@gmail.com>
2019-01-08 19:14:43 +0700

    Updates

---
+++ precache-manifest.827cb33ec54065b0f79c82b62319e5cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52026912b0dddad7dc67",
+    "url": "/thai-recog/static/js/runtime~main.52026912.js"
+  },
+  {
+    "revision": "ae12ccb96655ca6d87ee",
+    "url": "/thai-recog/static/js/main.ae12ccb9.chunk.js"
+  },
+  {
+    "revision": "b37b0e24dc7a40cb64e3",
+    "url": "/thai-recog/static/js/1.b37b0e24.chunk.js"
+  },
+  {
+    "revision": "ae12ccb96655ca6d87ee",
+    "url": "/thai-recog/static/css/main.df175e38.chunk.css"
+  },
+  {
+    "revision": "5349ee1b6c32e3a01680b07fc5c1ebea",
+    "url": "/thai-recog/index.html"
+  }
+];
